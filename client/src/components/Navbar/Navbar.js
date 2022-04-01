686401cef1b76a79a52b9831bf824d432d0a5b66
neo <developersmatrix3x3@gmail.com>
2022-04-01 17:25:24 +0530

    navbar updated

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,55 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 import classes from "./Navbar.module.css";
 import logo from "./logo.png";
 
 export const Navbar = () => {
+
+  const [navBarToggled, setNavBarToggled] = useState(false);
+
+  const toggleNavbar = () => {
+    setNavBarToggled((prevState) => {
+      return !prevState;
+    })
+  }
   return (
-    <header className={classes.header}>
-      {/* <!-- LOGO --> */}
-      <div className={classes.logo}>
-        <Link to="/" className={classes["logo-link"]}>
-          <img
-            src={logo}
-            alt="Town Panchayath Virajpet Logo"
-            className={classes["logo-img"]}
-          />
-          tp virajpet
-        </Link>
-      </div>
-      <nav className={classes["main-nav"]}>
-        <ul className={classes["nav-list"]}>
-          <li className={classes["nav-list__item"]}>
-            <Link className="nav-link" to="/">
-              DCB
-            </Link>
-          </li>
-          <li className={classes["nav-list__item"]}>
-            <Link className="nav-link" to="/">
-              Work Schemes
-            </Link>
-          </li>
-          <li className={classes["nav-list__item"]}>
-            <Link className="nav-link" to="/">
-              SCSP-TSP
-            </Link>
-          </li>
-          <li className={classes["nav-list__item"]}>
-            <Link className="nav-link" to="/">
-              Housing
-            </Link>
-          </li>
-          <li className={classes["nav-list__item"]}>
-            <Link className="nav-link" to="/">
-              Miscellaneous
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className={classes["nav-toggle"]}>
-        <div className={classes["hamburger"]}></div>
-      </div>
-    </header>
+    <Router>
+      <header className={classes.header}>
+        {/* <!-- LOGO --> */}
+        <div className={classes["brand-logo"]}>
+          <Link to="/" className={classes["brand-link"]}>
+            <img
+              src={logo}
+              alt="Town Panchayath Virajpet Logo"
+              className={classes["brand-img"]}
+            />
+            tp virajpet
+          </Link>
+        </div>
+        <nav className={navBarToggled ? `${classes["main-nav"]} ${classes["toggle-nav"]}` : classes["main-nav"]}>
+          <ul className={classes["nav-list"]}>
+            <li className={classes["nav-list__item"]}>
+              <Link className={classes["nav-link"]} to="/">
+                DCB
+              </Link>
+            </li>
+            <li className={classes["nav-list__item"]}>
+              <Link className={classes["nav-link"]} to="/">
+                Work Schemes
+              </Link>
+            </li>
+            <li className={classes["nav-list__item"]}>
+              <Link className={classes["nav-link"]} to="/">
+                SCSP-TSP
+              </Link>
+            </li>
+            <li className={classes["nav-list__item"]}>
+              <Link className={classes["nav-link"]} to="/">
+                Housing
+              </Link>
+            </li>
+            <li className={classes["nav-list__item"]}>
+              <Link className={classes["nav-link"]} to="/">
+                Miscellaneous
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={navBarToggled ? `${classes["nav-toggle"]} ${classes["toggle-ham"]}` : classes["nav-toggle"]}
+          onClick={toggleNavbar}
+        >
+          <div className={classes["hamburger"]}></div>
+        </div>
+      </header>
+    </Router>
   );
 };
