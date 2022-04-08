dfc41dd166e22c4c09119c2d638d02f931c56086
neo <developersmatrix3x3@gmail.com>
2022-04-08 22:23:32 +0530

    minor updated in updating dcb

---
+++ client/src/components/UpdateDCB/GetDataForm.js
@@ -0,0 +1,53 @@
+import { useState, useRef } from "react";
+
+import classes from "./GetDataForm.module.css";
+
+export const GetDataForm = () => {
+  const [collectionType, setCollectionType] = useState("");
+  const [financialYear, setFinancialYear] = useState("");
+
+  const ctypeRef = useRef(null);
+
+  const getDataHanler = (event) => {
+    event.preventDefault();
+    const date = new Date().getMonth();
+    console.log(date);
+    console.log(ctypeRef.current.value);
+  };
+
+  return (
+    <form onSubmit={getDataHanler}>
+      <div className={classes["container"]}>
+        <div className={classes["input"]}>
+          <label htmlFor="ctype">Collection Type</label>
+          <select
+            name="ctype"
+            ref={ctypeRef}
+            id="ctype"
+            className={classes.select}
+          >
+            <option value="">Select</option>
+            <option value="sas">Sas</option>
+            <option value="water">water</option>
+            <option value="trade">trade</option>
+            <option value="ad">Advertisement</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+        <div className={classes["input"]}>
+          <label htmlFor="year">Financial Year</label>
+          <select name="year" id="year" className={classes.select}>
+            <option value="">Select</option>
+            <option value="2022-23">2022-23</option>
+            <option value="2021-22">2021-22</option>
+            <option value="2020-21">2020-21</option>
+          </select>
+        </div>
+
+        <div>
+          <button className={classes["button"]}>Get Data</button>
+        </div>
+      </div>
+    </form>
+  );
+};
--- client/src/components/UpdateDCB/UpdateDCB.js
+++ client/src/components/UpdateDCB/UpdateDCB.js
@@ -1,54 +1,14 @@
 import React from "react";
 
+import { GetDataForm } from "./GetDataForm";
+
 import classes from "./UpdateDCB.module.css";
 
 export const UpdateDCB = () => {
   return (
-    <div>
-      <h1>Update Daily Collection Book</h1>
-      <form action="">
-        <div className={classes["input"]}>
-          <label htmlFor="ctype">Collection Type</label>
-          <select name="ctype" id="ctype" className={classes.select}>
-            <option value="">Select</option>
-            <option value="sas">Sas</option>
-            <option value="water">water</option>
-            <option value="trade">trade</option>
-            <option value="ad">Advertisement</option>
-            <option value="rent">Rent</option>
-          </select>
-        </div>
-        <div className={classes["input"]}>
-          <label htmlFor="year">financial Year</label>
-          <select name="year" id="year" className={classes.select}>
-            <option value="">Select</option>
-            <option value="2022-23">2022-23</option>
-            <option value="2021-22">2021-22</option>
-            <option value="2020-21">2020-21</option>
-          </select>
-        </div>
-        <div className={classes["input"]}>
-          <label htmlFor="month">Month</label>
-          <select name="month" id="month" className={classes.select}>
-            <option value="">Select</option>
-            <option value="april">April</option>
-            <option value="may">May</option>
-            <option value="june">June</option>
-            <option value="july">July</option>
-            <option value="august">August</option>
-            <option value="september">September</option>
-            <option value="october">October</option>
-            <option value="november">November</option>
-            <option value="december">December</option>
-            <option value="january">January</option>
-            <option value="february">February</option>
-            <option value="march">March</option>
-          </select>
-        </div>
-        <div>
-          <button>Get data</button>
-        </div>
-      </form>
+    <div className={classes["container"]}>
+      <h1 className={classes["title"]}>Update Daily Collection Book</h1>
+      <GetDataForm />
     </div>
   );
 };
